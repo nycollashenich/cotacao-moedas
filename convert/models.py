37f4168e55d0374fe4af782664abfcc49e64e986
Nycollas Henich <nhhenich@gmail.com>
2024-09-12 20:51:33 -0300

    bd config

--- convert/models.py
+++ convert/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Base(models.Model):
+    created_in = models.DateField('Criado em', auto_now_add=True)
+    modificated = models.DateField('Modificado em', auto_now=True)
+    active = models.BooleanField('Ativo?', default=True)
+
+    class Meta():
+        abstract = True
+
+class Conversion(Base):
+    from_currency = models.CharField(max_length=4)
+    to_currency = models.CharField(max_length=4)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    result = models.DecimalField(max_digits=10, decimal_places=2)
+    conversion_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.amount} {self.from_currency} to {self.to_currency}"
+    
+    
